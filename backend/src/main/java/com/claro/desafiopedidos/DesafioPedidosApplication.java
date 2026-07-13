83250f372a1d5bcfd7815f7a97d606d3f5eba345
ramonsilva186 <ramonsilva186@gmail.com>
2026-07-13 17:05:22 -0300

    project created

---
+++ backend/src/main/java/com/claro/desafiopedidos/DesafioPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.claro.desafiopedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPedidosApplication.class, args);
+	}
+
+}
